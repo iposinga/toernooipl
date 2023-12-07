f67e5dca2af1c732ceef9e02ff6dea5c604c878b
u24647p26455 <info@detoernooiplanner.nl>
2023-12-07 22:00:33 +0100

    version 7 december 22:00 uur

--- public/app_oud/inc/vars.php
+++
@@ -1,8 +0,0 @@
-<?
-	$username = "u24647p26455_toernooiplan";
-    $password = "osirules";
-	$host = "localhost";
-	$dbnaam = "u24647p26455_toernooiplan";
-	$db = mysqli_connect($host, $username, $password) or die (mysqli_error());
-	mysqli_select_db ($dbnaam, $db) or die (mysqli_error());
-?>
